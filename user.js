075c78a2eaf55b0bec704880594bc75f1c21d786
THUMILA <thumilakasthuricamp525@gmail.com>
2026-03-14 10:58:54 +0530

    When page load this code will run
    This code waits for the page to load and then sets up the login and register buttons so they work when clicked.

--- user.js
+++ user.js
@@ -2,3 +2,17 @@ const API_URL = "http://localhost/floodrelief";
 
 // Admin Password
 const ADMIN_PASSWORD = "admin1234";
+
+// When page loads
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', handleLogin);
+    }
+
+    if (registerForm) {
+        registerForm.addEventListener('submit', handleRegister);
+    }
+});
