8732df29bed3a92f650b068965c1f30eb53a9404
THUMILA <thumilakasthuricamp525@gmail.com>
2026-03-14 11:53:10 +0530

    added variables

--- login.php
+++ login.php
@@ -6,3 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     echo json_encode(["success" => false, "message" => "Method not allowed"]);
     exit;
 }
+
+$data = json_decode(file_get_contents("php://input"), true);
+$email = trim($data['email'] ?? '');
+$password = $data['password'] ?? '';
