31dc3d45c67be37be39a7e326f266aba97ccc63e
THUMILA <thumilakasthuricamp525@gmail.com>
2026-03-14 11:52:30 +0530

    database link added and Pods requset code added
    This code makes sure that the script only runs when the request is a POST request and stops with an error for any other method.

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+include 'db.php';
+include 'helpers.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode(["success" => false, "message" => "Method not allowed"]);
+    exit;
+}
